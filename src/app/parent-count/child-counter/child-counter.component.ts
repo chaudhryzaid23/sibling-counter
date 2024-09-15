fa969dd9a692d6034aeee1e86273991b4f3181d5
zaid hassan <chaudhryzaid23@gmail.com>
2024-09-15 10:19:00 +0500

    child counter with button

--- src/app/parent-count/child-counter/child-counter.component.ts
+++ src/app/parent-count/child-counter/child-counter.component.ts
@@ -5,8 +5,13 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './child-counter.component.html',
-  styleUrl: './child-counter.component.scss'
+  styleUrl: './child-counter.component.scss',
 })
 export class ChildCounterComponent {
+  count: number = 0;
 
+  onClick() {
+    this.count++;
+    console.log(this.count);
+  }
 }
